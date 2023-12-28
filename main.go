05142204caa318959350a0eeaca5fcfddbfbe2fc
AlexKimRav <ravenq1113@gmail.com>
2023-12-28 21:04:58 +0900

    router, main dependencies

--- main.go
+++ main.go
@@ -1,24 +1,40 @@
 package main
 
 import (
+	"jwtgogin/config"
+	"jwtgogin/controller"
 	"jwtgogin/helper"
+	"jwtgogin/model"
+	"jwtgogin/repository"
+	"jwtgogin/router"
+	"jwtgogin/service"
+	"log"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func main() {
 
-	router := gin.Default()
-	router.GET("", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, "hello world")
-	})
+	loadConfig, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal("could not load environment variables", err)
+	}
+	db := config.ConnectToDb(&loadConfig)
+	validate := validator.New()
+	db.Table("users").AutoMigrate(&model.Users{})
+
+	usersRepository := repository.NewUsersRepositoryImpl(db)
+	authenticationService := service.NewAuthenticationService(usersRepository, validate)
+	authenticationController := controller.NewAutheticationController(authenticationService)
+	routes := router.NewRouter(authenticationController)
+
 	server := &http.Server{
 		Addr:    ":8888",
-		Handler: router,
+		Handler: routes,
 	}
 
-	err := server.ListenAndServe()
-	helper.ErrorPanic(err)
+	server_err := server.ListenAndServe()
+	helper.ErrorPanic(server_err)
 
 }
